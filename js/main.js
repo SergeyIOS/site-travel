c91c0eb0e938ea5ed05d8ef4295f0d945e76b15f
SergeyIOS <64208971+SergeyIOS@users.noreply.github.com>
2021-09-03 10:15:16 +0300

    Travel

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function () {
+	$("a.scrol").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+
+  $('a.call-btn').click( function(event){
+    event.preventDefault();
+    $('#myOverlay').fadeIn(297,	function(){
+      $('#myModal') 
+      .css('display', 'block')
+      .animate({opacity: 1}, 198);
+    });
+  });
+
+  $('#myModal__close, #myOverlay').click( function(){
+    $('#myModal').animate({opacity: 0}, 198, function(){
+      $(this).css('display', 'none');
+      $('#myOverlay').fadeOut(297);
+    });
+  });
+
+  var btn = $('#button');  
+  $(window).scroll(function() {     
+    if ($(window).scrollTop() > 300) {
+       btn.addClass('show');
+     } else {
+       btn.removeClass('show');
+     }
+   });
+   btn.on('click', function(e) {
+     e.preventDefault();
+     $('html, body').animate({scrollTop:0}, '300');
+   });
+
+   $('.btn-routes').click(function (e) {
+   		e.preventDefault();
+   		$('.destination-hidden').toggleClass('destinations-visible')
+   }) 
+})
